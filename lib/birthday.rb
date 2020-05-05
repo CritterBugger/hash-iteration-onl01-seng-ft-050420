dc3f9b71a7966a4a92362a0f9dcea481401600e3
Luke Chen <luke.chen.2010@gmail.com>
2020-05-05 19:26:59 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,8 +12,7 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!" if age <= 12
-  end
+    age <= 12? puts "Happy Birthday #{kids_name}! You are now #{age} years old!" : puts "You are too old for this."
 end
 
 
